a46f3e07e6c860fec2455d3bead44bf6c99b57b5
thefork-widgets <widgetsfb@thefork.com>
2017-01-26 14:44:45 +0100

    Update changeURL.js

--- changeURL.js
+++ changeURL.js
@@ -1,25 +1,16 @@
-/**************************************************************/
-/*  Change the weburl after thankyou page. Widget spanish only*/
-/*  works if and iframe is inside a website                   */ 
-/**************************************************************/
-
 <script type="text/javascript">
-
-
-if (typeof String.prototype.startsWith != 'function') {
-  String.prototype.startsWith = function (str){
-    return this.indexOf(str) == 0;
-  };
-}     
-
+  
 var pathname = window.location.pathname;
 var url = window.location.href;
+console.log(pathname);
+console.log(url);
      
      console.log(pathname);
      console.log(window.location);
     
-      if(url.startsWith("https://module.lafourchette.com/es_ES/") && pathname.search( 'thank-you' )!=-1){
-          window.top.location.href= "  URL  "; //url to open
+      if(pathname.search( 'thank-you' )!=-1){
+            console.log("OK");          
+            window.top.location.href= "http://URL";
           
       }
 </script>
